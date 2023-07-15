3c3f3b87e81b487bc58d1974ed2640c524f0f729
Joe Reed <joereed.dev@gmail.com>
2023-07-15 16:34:17 +0100

    Install and run prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import { Label } from "@/components/ui/Label"
-import { Input } from "@/components/ui/Input"
+import { Label } from "@/components/ui/Label";
+import { Input } from "@/components/ui/Input";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-24">
-
-      <Label htmlFor="name" className="mb-3">Add a participant</Label>
+      <Label htmlFor="name" className="mb-3">
+        Add a participant
+      </Label>
       <Input id="name" />
     </main>
-  )
+  );
 }
