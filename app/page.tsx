9fdafd599010f1e1b9136052b32f32b2214b46c4
Joe Reed <joereed.dev@gmail.com>
2023-07-15 18:19:02 +0100

    Rotate participants on a timer

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,25 @@ import { Button } from "@/components/ui/button";
 import dynamic from "next/dynamic";
 import { X } from "lucide-react";
 
+const WORKING_TIME = 25 * 60;
+const BREAK_TIME = 5 * 60;
+
 function Home() {
   const [name, setName] = React.useState("");
   const [participants, setParticipants] = React.useState<string[]>(
     JSON.parse(localStorage.getItem("participants") || "[]"),
+  );
+  const [currentParticipant, setCurrentParticipant] = React.useState<
+    string | null
+  >(localStorage.getItem("currentParticipant") || participants[0]);
+  const [timerRunning, setTimerRunning] = React.useState<boolean>(
+    JSON.parse(localStorage.getItem("timerRunning") || "false"),
+  );
+  const [timeRemaining, setTimeRemaining] = React.useState<number>(
+    parseInt(localStorage.getItem("timeRemaining") || WORKING_TIME.toString()),
+  );
+  const [onBreak, setOnBreak] = React.useState<boolean>(
+    JSON.parse(localStorage.getItem("onBreak") || "false"),
   );
 
   function addParticipant(e: React.FormEvent<HTMLFormElement>) {
@@ -24,48 +39,140 @@ function Home() {
     setName("");
 
     localStorage.setItem("participants", JSON.stringify(newParticipants));
+
+    if (!currentParticipant) {
+      updateCurrentParticipant(name);
+    }
   }
 
   function removeParticipant(participant: string) {
+    const participantIndex = participants.indexOf(participant);
+
     const newParticipants = participants.filter((p) => p !== participant);
     setParticipants(newParticipants);
     localStorage.setItem("participants", JSON.stringify(newParticipants));
+
+    if (currentParticipant === participant) {
+      updateCurrentParticipant(
+        newParticipants[participantIndex] ||
+          newParticipants[participantIndex - 1] ||
+          null,
+      );
+    }
   }
+
+  function rotateCurrentParticipant() {
+    if (!participants.length) return;
+
+    const currentParticipantIndex = currentParticipant
+      ? participants.indexOf(currentParticipant)
+      : 0;
+    const nextParticipantIndex = currentParticipantIndex + 1;
+
+    const nextParticipant =
+      nextParticipantIndex >= participants.length
+        ? participants[0]
+        : participants[nextParticipantIndex];
+
+    updateCurrentParticipant(nextParticipant);
+  }
+
+  function updateCurrentParticipant(participant: string | null) {
+    setCurrentParticipant(participant);
+    localStorage.setItem("currentParticipant", participant || "");
+  }
+
+  function updateTimeRemaining(newTimeRemaining: number) {
+    setTimeRemaining(newTimeRemaining);
+    localStorage.setItem("timeRemaining", newTimeRemaining.toString());
+  }
+
+  function toggleTimerRunning() {
+    setTimerRunning(!timerRunning);
+    localStorage.setItem("timerRunning", JSON.stringify(!timerRunning));
+  }
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      if (!timerRunning) return;
+
+      let newTimeRemaining = timeRemaining - 1;
+
+      if (timeRemaining <= 0) {
+        if (onBreak) {
+          setOnBreak(false);
+          rotateCurrentParticipant();
+          newTimeRemaining = WORKING_TIME;
+        } else {
+          setOnBreak(true);
+          newTimeRemaining = BREAK_TIME;
+        }
+      }
+
+      updateTimeRemaining(newTimeRemaining);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
 
   return (
     <main className="flex min-h-screen flex-col p-24">
       <h1 className="text-4xl font-bold mb-8">Pomobdoro</h1>
-      <form onSubmit={addParticipant} className="mb-5">
-        <div className="flex space-x-4 items-end">
-          <div className="flex flex-col">
-            <Label htmlFor="name" className="mb-3">
-              Add a participant
-            </Label>
-            <Input
-              id="name"
-              name="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <Button type="submit">Add</Button>
-        </div>
-      </form>
 
-      <ul className="flex flex-col space-y-1">
-        {participants.map((participant) => (
-          <li key={participant}>
-            <div className="flex items-center">
-              <p className="mr-4">{participant}</p>
-              <X
-                className="mt-[1px]"
-                size={16}
-                onClick={() => removeParticipant(participant)}
-              />
+      <div className="flex">
+        <div className="mr-32">
+          <form onSubmit={addParticipant} className="mb-5">
+            <div className="flex space-x-4 items-end">
+              <div className="flex flex-col">
+                <Label htmlFor="name" className="mb-3">
+                  Add a participant
+                </Label>
+                <Input
+                  id="name"
+                  name="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <Button type="submit">Add</Button>
             </div>
-          </li>
-        ))}
-      </ul>
+          </form>
+
+          <ul className="flex flex-col space-y-1">
+            {participants.map((participant) => (
+              <li key={participant}>
+                <div className="flex items-center">
+                  <p className="mr-4">{participant}</p>
+                  <X
+                    className="mt-[1px]"
+                    size={16}
+                    onClick={() => removeParticipant(participant)}
+                  />
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          <p className="text-4xl font-bold mb-8">{`${Math.floor(
+            (timeRemaining % 3600) / 60,
+          )
+            .toString()
+            .padStart(2, "0")}:${Math.floor(timeRemaining % 60)
+            .toString()
+            .padStart(2, "0")}`}</p>
+          <p className="text-2xl font-bold mb-8">
+            Current Participant: {currentParticipant}
+          </p>
+          <p className="text-2xl font-bold mb-8">
+            {onBreak ? "On break" : "Working"}
+          </p>
+          <Button onClick={toggleTimerRunning}>
+            {timerRunning ? "Pause" : "Start"}
+          </Button>
+        </div>
+      </div>
     </main>
   );
 }
